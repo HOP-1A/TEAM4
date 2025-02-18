289ea0e20a76530283fb1b7fd910eafc6ef4c76a
24HP6298 24HP6298 <24HP6298@admins-iMac.local>
2025-02-18 00:49:38 -0800

    profileInput

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,6 +1,12 @@
+<<<<<<< HEAD
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
+=======
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+>>>>>>> cd82f41 (profileInput)
 
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
   ({ className, type, ...props }, ref) => {
@@ -8,15 +14,28 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
       <input
         type={type}
         className={cn(
+<<<<<<< HEAD
           "flex h-9 w-full rounded-md rounded-tl-none rounded-bl-none border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+=======
+          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+>>>>>>> cd82f41 (profileInput)
           className
         )}
         ref={ref}
         {...props}
       />
+<<<<<<< HEAD
     );
   }
 );
 Input.displayName = "Input";
 
 export { Input };
+=======
+    )
+  }
+)
+Input.displayName = "Input"
+
+export { Input }
+>>>>>>> cd82f41 (profileInput)
