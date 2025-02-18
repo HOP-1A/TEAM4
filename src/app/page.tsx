d3bd05701489488de700b77cbbf6925cd61c2ec0
Taffyyy123 <tsolmn9@gmail.com>
2025-02-18 16:20:10 +0800

    navBar hiisen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import ProductPanelSmall from "@/_components/ProductPanelSmall";
+import { NavBar } from "@/custom-components/NavBar";
 
 export default function Home() {
   const products = {
@@ -9,13 +10,16 @@ export default function Home() {
       " https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D",
   };
   return (
-    <div className="w-[150px] h-[300px]">
-      <ProductPanelSmall
-        price={products.price}
-        date={products.date}
-        description={products.description}
-        imgUrl={products.imgUrl}
-      />
+    <div>
+      <NavBar />
+      <div className="w-[150px] h-[300px]">
+        <ProductPanelSmall
+          price={products.price}
+          date={products.date}
+          description={products.description}
+          imgUrl={products.imgUrl}
+        />
+      </div>
     </div>
   );
 }
