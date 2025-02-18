583888a5a2904851de176f789ac4bf6c2fbdc2d8
24HP6298 24HP6298 <24HP6298@admins-iMac.local>
2025-02-18 01:31:07 -0800

    UpdatedProfileInputs

--- src/app/settings/_components/ProfileInputs.tsx
+++ src/app/settings/_components/ProfileInputs.tsx
@@ -1,24 +1,89 @@
+"use client";
 import { Input } from "@/components/ui/input";
-const ProfileInputs = () => {
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+type ProfileInputValuesProp = {
+  name: string;
+  surname: string;
+  mail: string;
+  phoneNumber: string;
+};
+export default function ProfileInputs({
+  name,
+  surname,
+  mail,
+  phoneNumber,
+}: ProfileInputValuesProp) {
+  const [nameValue, setNameValue] = useState(name);
+  const [surnameValue, setSurnameValue] = useState(surname);
+  const [mailValue, setMailValue] = useState(mail);
+  const [phoneNumberValue, setPhoneNumberValue] = useState(phoneNumber);
+  const SaveUserInfoChange = () => {
+    console.log(
+      nameValue +
+        "||" +
+        surnameValue +
+        "||" +
+        mailValue +
+        "||" +
+        phoneNumberValue
+    );
+  };
+  const HandleNameValue = (e: { target: { value: string } }) => {
+    setNameValue(e.target.value);
+  };
+  const HandleSurnameValue = (e: { target: { value: string } }) => {
+    setSurnameValue(e.target.value);
+  };
+  const HandleMailValue = (e: { target: { value: string } }) => {
+    setMailValue(e.target.value);
+  };
+  const HandlePhoneNumberValue = (e: { target: { value: string } }) => {
+    setPhoneNumberValue(e.target.value);
+  };
   return (
-    <div className="w-[500px] flex flex-wrap gap-5">
-      <div>
-        <p className="text-red-600 text-sm">* Нэр</p>
-        <Input className="w-[200px]" />
+    <div>
+      <div className="w-[500px] flex flex-wrap gap-5">
+        <div>
+          <p className="text-red-600 text-sm">* Нэр</p>
+          <Input
+            className="w-[200px]"
+            value={nameValue}
+            onChange={HandleNameValue}
+          />
+        </div>
+        <div>
+          <p className="text-red-600 text-sm">* Эцэг/Эх-ийн нэр</p>
+          <Input
+            className="w-[200px]"
+            value={surnameValue}
+            onChange={HandleSurnameValue}
+          />
+        </div>
+        <div>
+          <p className="text-red-600 text-sm">* Mail</p>
+          <Input
+            className="w-[200px]"
+            value={mailValue}
+            onChange={HandleMailValue}
+          />
+        </div>
+
+        <div>
+          <p className="text-red-600 text-sm">* Phone Number</p>
+          <Input
+            className="w-[200px]"
+            value={phoneNumberValue}
+            onChange={HandlePhoneNumberValue}
+          />
+        </div>
       </div>
-      <div>
-        <p className="text-red-600 text-sm">* Эцэг/Эх-ийн нэр</p>
-        <Input className="w-[200px]" />
-      </div>
-      <div>
-        <p className="text-red-600 text-sm">* Mail</p>
-        <Input className="w-[200px]" />
-      </div>
-      <div>
-        <p className="text-red-600 text-sm">* Phone Number</p>
-        <Input className="w-[200px]" />
-      </div>
+      <Button
+        className="bg-black text-white w-[100px]"
+        onClick={SaveUserInfoChange}
+      >
+        Save
+      </Button>
     </div>
   );
-};
-export default ProfileInputs;
+}
