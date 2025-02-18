0fcd0e6cf592449d0f0c1c9615c93de359ec7223
Taffyyy123 <tsolmn9@gmail.com>
2025-02-18 18:33:08 +0800

    header done

--- src/custom-components/NavBar.tsx
+++ src/custom-components/NavBar.tsx
@@ -20,18 +20,18 @@ import {
 
 export const NavBar = () => {
   return (
-    <div className="w-screen flex flex-col items-center gap-12">
-      <div className="bg-red-500 w-screen h-[50px] flex justify-around items-center text-white">
-        <div className="font-bold text-lg">Хүссэнээ зар, Хайснаа ол</div>
+    <div className="w-screen flex flex-col items-center gap-12 fixed top-0">
+      <div className="bg-red-500 w-screen h-[30px] flex justify-around items-center text-white">
+        <div className="font-bold text-md">Хүссэнээ зар, Хайснаа ол</div>
         <div className="flex gap-8">
           <div className="flex gap-2 border-r-2 pr-8">
             <Phone />
             <div>99999999</div>
           </div>
           <div className="flex gap-4">
-            <Facebook />
-            <Instagram />
-            <Twitter />
+            <Facebook className="hover:cursor-pointer" />
+            <Instagram className="hover:cursor-pointer" />
+            <Twitter className="hover:cursor-pointer" />
           </div>
         </div>
       </div>
@@ -91,13 +91,13 @@ export const NavBar = () => {
             </button>
           </div>
           <div className="flex items-center" style={{ gap: 12 }}>
-            <div className="bg-neutral-100 rounded-3xl w-12 h-12 flex justify-center items-center">
-              <Heart fill="red" className="text-red-500" />
+            <div className="bg-neutral-100 rounded-3xl w-12 h-12 flex justify-center items-center hover:cursor-pointer">
+              <Heart fill="red" className="text-red-500 " />
             </div>
-            <div className="bg-neutral-100 rounded-3xl w-12 h-12 flex justify-center items-center">
-              <ShoppingBasket fill="red" className="text-red-500" />
+            <div className="bg-neutral-100 rounded-3xl w-12 h-12 flex justify-center items-center hover:cursor-pointer">
+              <ShoppingBasket fill="red" className="text-red-500 " />
             </div>
-            <div className="bg-neutral-100 rounded-3xl w-12 h-12 flex justify-center items-center">
+            <div className="bg-neutral-100 rounded-3xl w-12 h-12 flex justify-center items-center hover:cursor-pointer">
               <User fill="red" className="text-red-500" />
             </div>
           </div>
@@ -106,14 +106,20 @@ export const NavBar = () => {
       <div className="w-screen flex justify-center border-t-2 border-b-2 border-neutral-200 pt-6 pb-6">
         <div className="flex justify-around w-3/4">
           <div className="flex gap-8">
-            <div className="flex gap-2">
+            <div className="flex gap-2 hover:cursor-pointer">
               <AlignJustify className="text-sm" />
               <div className="font-bold">Ангилал</div>
             </div>
-            <div className="text-neutral-600">Бараа бүтээгдэхүүнүүд</div>
-            <div className="text-neutral-600">Хэрэглэгчийн тусламж</div>
+            <div className="text-neutral-600 hover:cursor-pointer">
+              Бараа бүтээгдэхүүнүүд
+            </div>
+            <div className="text-neutral-600 hover:cursor-pointer">
+              Хэрэглэгчийн тусламж
+            </div>
           </div>
-          <div className="text-neutral-600">Шинэ бараа</div>
+          <div className="text-neutral-600 hover:cursor-pointer">
+            Шинэ бараа
+          </div>
         </div>
       </div>
     </div>
