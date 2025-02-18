b91939fadcce1613c734625568fc85db0da1b849
24HP6298 24HP6298 <24HP6298@admins-iMac.local>
2025-02-18 00:56:37 -0800

    ProfileInputs

---
+++ src/app/settings/_components/ProfileInputs.tsx
@@ -0,0 +1,24 @@
+import { Input } from "@/components/ui/input";
+const ProfileInputs = () => {
+  return (
+    <div className="w-[500px] flex flex-wrap gap-5">
+      <div>
+        <p className="text-red-600 text-sm">* Нэр</p>
+        <Input className="w-[200px]" />
+      </div>
+      <div>
+        <p className="text-red-600 text-sm">* Эцэг/Эх-ийн нэр</p>
+        <Input className="w-[200px]" />
+      </div>
+      <div>
+        <p className="text-red-600 text-sm">* Mail</p>
+        <Input className="w-[200px]" />
+      </div>
+      <div>
+        <p className="text-red-600 text-sm">* Phone Number</p>
+        <Input className="w-[200px]" />
+      </div>
+    </div>
+  );
+};
+export default ProfileInputs;
