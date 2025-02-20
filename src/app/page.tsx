54335bc8cfde115c23192e91f7d532209a39d645
Taffyyy123 <tsolmn9@gmail.com>
2025-02-20 15:17:20 +0800

    d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { NavBar } from "@/custom-components/NavBar";
 import { Footer } from "./_components";
 export default function Home() {
   return (
-    <div className="relative">
+    <div>
       <NavBar />
       <Footer/>
       
