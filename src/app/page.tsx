36f68a4cf102748177b7f883d99c41a3181cac20
Taffyyy123 <tsolmn9@gmail.com>
2025-02-20 15:24:02 +0800

    h

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import ProductPanelSmall from "@/_components/ProductPanelSmall";
+import ProfileInputs from "./settings/_components/ProfileInputs";
 import { NavBar } from "@/custom-components/NavBar";
 import { Footer } from "./_components";
 export default function Home() {
   return (
-    <div>
+    <div className="relative">
       <NavBar />
       <Footer/>
     </div>
