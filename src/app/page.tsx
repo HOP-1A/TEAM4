bbff16cf218aeb8dddf1271c1b708e74c0e43637
Taffyyy123 <tsolmn9@gmail.com>
2025-02-20 17:24:17 +0800

    favUsersPanelSmall duussan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { FavUsersPanelSmall } from "@/_components/FavUsersPanelSmall";
 import ProductPanelSmall from "@/_components/ProductPanelSmall";
 import { NavBar } from "@/custom-components/NavBar";
 
@@ -9,6 +10,12 @@ export default function Home() {
     imgUrl:
       " https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D",
   };
+  const favUser = {
+    imgUrl: "https://cdnp.cody.mn/vendor/57261/medium/1000000053.jpg",
+    lastName: "Bike",
+    firstName: "chain",
+    createdAt: "2025/1/09",
+  };
   return (
     <div className="relative">
       <NavBar />
@@ -18,6 +25,14 @@ export default function Home() {
           date={products.date}
           description={products.description}
           imgUrl={products.imgUrl}
+        />
+      </div>
+      <div>
+        <FavUsersPanelSmall
+          imgUrl={favUser.imgUrl}
+          lastName={favUser.lastName}
+          firstName={favUser.firstName}
+          createdAt={favUser.createdAt}
         />
       </div>
     </div>
