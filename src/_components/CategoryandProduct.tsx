e02dfb1f868a71d75d367286a3c59aa704fa7a19
24HP6902 24HP6902 <24HP6902@admins-iMac.local>
2025-02-22 12:53:09 +0800

    done

---
+++ src/_components/CategoryandProduct.tsx
@@ -0,0 +1,16 @@
+import { ProductPanelSmall } from "@/app/_components/ProductPanelSmall";
+
+export const Product = () => {
+  return (
+    <div>
+      <div>
+        <div>Найз охиндоо</div>
+        
+      </div>
+      <div>
+        <div>Найз залуудаа</div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,26 +1,28 @@
 export const Footer = () => {
-    return (
-        <div className="w-[100vw]">
-            <div className="flex bg-pink-500">
-                <div className="">
-                <div className="py-50  py-[2vw]  pl-[20vw] flex justify-between w-[100vw]">
-                <img className="w-[20vw]" src="https://zary.mn/d1bbe99a59718a190b84bd47d0787533.png"/>
-                <div className="py-50  py-[2vw]">
-                <div>Холбоо барих</div>
-                <div>Улаан баатар хот, Сүх баатар дүүрэг, 1-р</div>
-                <div>хороо, Парисын гудамж 42, IC Tower, 10</div>
-                <div>давхар</div>
-                <div>Утас:  7777-8984</div>
-                <div>Имэйл: info@zary.mn</div>
+  return (
+    <div className="w-[100vw] flex items-end h-[100vh]">
+      <div className="flex bg-pink-500 ">
+        <div className="">
+          <div className="py-50  py-[2vw]  pl-[20vw] flex justify-between w-[100vw]">
+            <img
+              className="w-[10vw] h-[10vh]"
+              src="https://zary.mn/d1bbe99a59718a190b84bd47d0787533.png"
+            />
+            <div className="py-50  py-[2vw] pr-[30vw]">
+              <div>Холбоо барих</div>
+              <div>Улаан баатар хот, Сүх баатар дүүрэг, 1-р</div>
+              <div>хороо, Парисын гудамж 42, IC Tower, 10</div>
+              <div>давхар</div>
+              <div>Утас: 7777-8984</div>
+              <div>Имэйл: info@zary.mn</div>
             </div>
-                </div>
-                <div className="flex pb-[2vw] pl-[20vw] pt-[10vh] gap-[30vw]">
+          </div>
+          <div className="flex pb-[2vw] pl-[20vw] pt-[10vh] gap-[30vw]">
             <div className="">© 2025 Бүх эрх хуулиар хамгаалагдсан.</div>
             <div>Developed by Cody LLC</div>
-            </div>
-            </div>
-            
-            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
