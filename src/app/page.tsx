4d585cf0f046e7aeaa67f503763f172d14196481
Taffyyy123 <tsolmn9@gmail.com>
2025-02-22 11:54:30 +0800

    productinfo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import { NavBar } from "@/custom-components/NavBar";
 import { Footer } from "./_components";
-import { ProductPanelSmall } from "./_components/ProductPanelSmall";
-
 export default function Home() {
   return (
     <div className="relative">
