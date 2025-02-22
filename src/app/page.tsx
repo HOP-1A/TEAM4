98393d7c9a5bb92b48ab1e8be4766924aab9f034
24HP6902 24HP6902 <24HP6902@admins-iMac.local>
2025-02-22 10:55:30 +0800

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,11 @@
-<<<<<<< Updated upstream
-import ProductPanelSmall from "@/_components/ProductPanelSmall";
 import { NavBar } from "@/custom-components/NavBar";
-=======
 import { Footer } from "./_components";
->>>>>>> Stashed changes
 
 export default function Home() {
   return (
-<<<<<<< Updated upstream
     <div className="relative">
       <NavBar />
-      <div className="w-[150px] h-[300px]">
-        <ProductPanelSmall
-          price={products.price}
-          date={products.date}
-          description={products.description}
-          imgUrl={products.imgUrl}
-        />
-      </div>
-=======
-    <div className="w-[150px] h-[300px]">
       <Footer/>
->>>>>>> Stashed changes
     </div>
   );
 }
