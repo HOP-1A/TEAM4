3187a8cb403b47b5330f62c0f4e65969750afef6
Taffyyy123 <tsolmn9@gmail.com>
2025-02-25 17:28:16 +0800

    newProduct

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import { NavBar } from "@/custom-components/NavBar";
 import { Footer } from "./_components";
+import { ShowNewProducts } from "./_components/ShowNewProducts";
 import CategoryList from "@/_components/CategoryList";
 
 export default function Home() {
   return (
     <div className="relative">
-      <NavBar />
-      <Footer />
-      <CategoryList />
+      <ShowNewProducts />
     </div>
   );
 }
