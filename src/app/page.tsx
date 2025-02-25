e40a3a302bc01d4f61aa3430bfa71a64c0c556fb
24HP5566 24HP5566 <24HP5566@admins-iMac.local>
2025-02-25 15:08:01 +0800

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import ProductPanelSmall from "@/_components/ProductPanelSmall";
-import ProfileInputs from "./settings/_components/ProfileInputs";
 import { NavBar } from "@/custom-components/NavBar";
 import { Footer } from "./_components";
 export default function Home() {
